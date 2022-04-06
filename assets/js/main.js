f7308570c2f945bd2fbd379a4ba7b1b545258da3
Belal khan <belalkhan.dev@gmail.com>
2022-04-06 16:26:40 +0600

    design automoto

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function($){
+    "use-strict"
+
+    //on document ready
+    jQuery(document).on('ready', function() {
+        //
+    });
+
+    //on windown load
+    jQuery(window).on('load', function() {
+        //
+    });
+
+}(jQuery))
